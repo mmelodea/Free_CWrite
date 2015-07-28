e4b91c32c49a334dab0ac847d19bb3a3c05413c1
mmelodea <mmelodea@cern.ch>
2015-07-27 22:16:37 -0300

    shell script example

---
+++ extract_limits.sh
@@ -0,0 +1,35 @@
+cd 170/
+combine -M Asymptotic -m 170 limits170.txt > limits_results170.txt
+
+cd ../180/
+combine -M Asymptotic -m 180 limits180.txt > limits_results180.txt
+
+cd ../190/
+combine -M Asymptotic -m 190 limits190.txt > limits_results190.txt
+
+cd ../200/
+combine -M Asymptotic -m 200 limits200.txt > limits_results200.txt
+
+cd ../250/
+combine -M Asymptotic -m 250 limits250.txt > limits_results250.txt
+
+cd ../300/
+combine -M Asymptotic -m 300 limits300.txt > limits_results300.txt
+
+cd ../350/
+combine -M Asymptotic -m 350 limits350.txt > limits_results350.txt
+
+cd ../400/
+combine -M Asymptotic -m 400 limits400.txt > limits_results400.txt
+
+cd ../450/
+combine -M Asymptotic -m 450 limits450.txt > limits_results450.txt
+
+cd ../500/
+combine -M Asymptotic -m 500 limits500.txt > limits_results500.txt
+
+cd ../550/
+combine -M Asymptotic -m 550 limits550.txt > limits_results550.txt
+
+cd ../600/
+combine -M Asymptotic -m 600 limits600.txt > limits_results600.txt
